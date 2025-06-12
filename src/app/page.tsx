0ef278c52d24dca4cdefe7b24d3adbd1771ace64
Raffi Jacobs <raffij@gmail.com>
2025-06-12 10:23:04 +0100

    Update text sizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-lvh justify-center items-center">
-      <h1 className="text-4xl">JINK BAKERY & DELI</h1>
+    <main className="flex flex-col min-h-lvh justify-center items-center text-center">
+      <h1 className="text-3xl md:text-4xl">JINK BAKERY & DELI</h1>
       <p>
         <i>Coming soon</i>
       </p>
