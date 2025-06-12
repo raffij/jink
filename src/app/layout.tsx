774c45a6a0907e70ffd5d7b22b316a030a42f973
Raffi Jacobs <raffij@gmail.com>
2025-06-12 09:08:46 +0100

    Initial jink site

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Permanent_Marker } from "next/font/google";
+import "./globals.css";
+
+const ebGaramondSans = Permanent_Marker({
+  variable: "--font-eb-garamond",
+  weight: '400',
+  
+});
+
+export const metadata: Metadata = {
+  title: "Jink Bakery & Deli",
+  description: "Coming soon",
+};
+
+// #db66ed #e68257 #f0ea3e
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${ebGaramondSans.variable} ${ebGaramondSans.variable} antialiased`}
+        style={{ background: "#f0ea3e" }}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <main className="flex flex-col min-h-lvh justify-center items-center">
+      <h1 className="text-4xl">JINK BAKERY & DELI</h1>
+      <p>
+        <i>Coming soon</i>
+      </p>
+    </main>
+  );
+}
