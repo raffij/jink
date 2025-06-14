db4ac3efebcff66e4cd134878eec7167f17b3c79
Raffi Jacobs <raffij@gmail.com>
2025-06-14 07:49:12 +0100

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,39 @@
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [showComingSoon, setShowComingSoon] = useState(false);
+
+  const handleClick = () => {
+    setShowComingSoon((s) => !s);
+  };
+
   return (
-    <main className="flex flex-col min-h-dvh justify-center items-center text-center">
-      <h1 className="text-3xl md:text-4xl">JINK BAKERY & DELI</h1>
-      <p>
-        <i>Coming soon</i>
-      </p>
+    <main
+      className="flex flex-col min-h-dvh justify-center items-center text-center cursor-pointer p-5"
+      onClick={handleClick}
+    >
+      {showComingSoon ? (
+        <>
+          <p>
+            <i>Coming soon</i>
+          </p>
+        </>
+      ) : (
+        <>
+          <img
+            src="/sitting1.png"
+            className="w-full max-w-[600px] m-auto select-none block md:hidden"
+            alt="Jink Bakery and Deli - Coming soon"
+          />
+          <img
+            src="/sleeping1.png"
+            className="w-full max-w-[600px] m-auto select-none hidden md:block"
+            alt="Jink Bakery and Deli - Coming soon"
+          />
+        </>
+      )}
     </main>
   );
 }
