a4be9a7fe94729f09b27c3db13f37ca13d8c6ffb
Raffi Jacobs <raffij@gmail.com>
2025-06-14 06:56:08 +0100

    Update page.tsx

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,22 +1,16 @@
-'use client'
-
-import { useState } from "react";
-
-const images = ['/sitting1.png', '/sleeping1.png', '/laying1.png'];
-
 export default function Home() {
-  const [currentImage, setCurrentImage ] = useState(0);
-
-  const handleImageChange = () => {
-    setCurrentImage(s => s+1 === images.length ? 0 : s+1);
-  }
-
+  
   return (
     <main className="flex flex-col min-h-dvh justify-center items-center text-center">
-      <div onClick={handleImageChange} className="p-5">
+      <div className="p-5">
         <img
-          src={images[currentImage]}
-          className="w-full max-w-[600px] m-auto select-none"
+          src="/sitting1.png"
+          className="w-full max-w-[600px] m-auto select-none block md:hidden"
+          alt="Jink Bakery and Deli - Coming soon"
+        />
+        <img
+          src="/sleeping1.png"
+          className="w-full max-w-[600px] m-auto select-none hidden md:block"
           alt="Jink Bakery and Deli - Coming soon"
         />
       </div>
