1fe077cc729e0e78a07aa1e84d1f30c0780ee8fe
Raffi Jacobs <raffij@gmail.com>
2025-06-14 07:28:05 +0100

    Update page.tsx

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,19 +1,35 @@
+'use client'
+
+import { useState } from "react";
+
 export default function Home() {
-  
+  const [showComingSoon, setShowComingSoon] = useState(false);
+
+  const handleClick = () => {
+    setShowComingSoon(true);
+  };
+
   return (
     <main className="flex flex-col min-h-dvh justify-center items-center text-center">
-      <div className="p-5">
-        <img
-          src="/sitting1.png"
-          className="w-full max-w-[600px] m-auto select-none block md:hidden"
-          alt="Jink Bakery and Deli - Coming soon"
-        />
-        <img
-          src="/sleeping1.png"
-          className="w-full max-w-[600px] m-auto select-none hidden md:block"
-          alt="Jink Bakery and Deli - Coming soon"
-        />
-      </div>
+      {showComingSoon ?
+        <>
+          <h1 className="text-3xl md:text-4xl">JINK BAKERY & DELI</h1>
+          <p>
+            <i>Coming soon</i>
+          </p>
+        </> : <div className="p-5" onClick={handleClick}>
+          <img
+            src="/sitting1.png"
+            className="w-full max-w-[600px] m-auto select-none block md:hidden"
+            alt="Jink Bakery and Deli - Coming soon"
+          />
+          <img
+            src="/sleeping1.png"
+            className="w-full max-w-[600px] m-auto select-none hidden md:block"
+            alt="Jink Bakery and Deli - Coming soon"
+          />
+        </div>
+    }  
     </main>
   );
 }
