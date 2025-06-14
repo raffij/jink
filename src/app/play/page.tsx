6c950d1c512aefdcf4412b7d5c41e3fe65c39361
Raffi Jacobs <raffij@gmail.com>
2025-06-14 07:44:17 +0100

    Update page.tsx

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -13,7 +13,6 @@ export default function Home() {
     <main className="flex flex-col min-h-dvh justify-center items-center text-center">
       {showComingSoon ?
         <>
-          <h1 className="text-3xl md:text-4xl">JINK BAKERY & DELI</h1>
           <p>
             <i>Coming soon</i>
           </p>
